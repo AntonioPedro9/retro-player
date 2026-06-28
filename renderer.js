aa79445f6cffe4f00c7e01ab352d0ce4960ae25e
Antônio Pedro <antoniopedro.rs9@gmail.com>
2026-06-28 16:16:45 -0300

    feat: add drag bar

--- renderer.js
+++
@@ -1,4 +0,0 @@
-const information = document.getElementById("info");
-if (information && window.versions) {
-  information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`;
-}
